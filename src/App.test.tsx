4a6e6931b42834acd328fe442727a973d275b1b6
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-06-12 13:07:11 +0300

    Pass.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,8 +5,6 @@ import App from "./App";
 it("renders without crashing", () => {
   const div = document.createElement("div");
 
-  throw Error;
-
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
